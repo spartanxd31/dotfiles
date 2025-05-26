8a385cfc87dc2fd3e49161e5fa9508b300e16b5e
Domenic Marcelli <spartanxd31@vt.edu>
2025-05-26 14:27:47 -0400

    Initial Commit

---
+++ hypr/scripts/wallpapers.sh
@@ -0,0 +1,15 @@
+
+
+#kill hyprpaper?
+
+#killall hyprpaper
+
+#launch hyprpaper
+
+#hyprpaper
+
+#load the wallpaper
+
+hyprctl hyprpaper preload "/home/domenic/Pictures/PurpleMountains.jpg"
+
+hyprctl hyprpaper wallpaper ",/home/domenic/Pictures/PurpleMountains.jpg"
