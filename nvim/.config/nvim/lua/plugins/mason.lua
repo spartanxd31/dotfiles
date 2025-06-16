fbb9a61fd64d11fd5c8c190be30d698421525da2
spartanxd31 <spartanxd31@vt.edu>
2025-06-15 23:53:05 -0400

    Moving towards gruvbox themeing for now

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,8 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = {},           -- don't auto-install
+    automatic_installation = false, -- disable Mason install
+  },
+}
+
